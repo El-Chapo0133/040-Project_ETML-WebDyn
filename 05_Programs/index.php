da44b0888dec049ea79196cb994411ec58faa972
Levêque Loris <levequelo@etml.educanet2.ch>
2019-03-11 14:07:27 +0100

    Made the program organization
    we decided to stay the folders linked from bootstrap with the file, but named with a 99 and 98 before them

--- 05_Programs/index.php
+++ 05_Programs/index.php
@@ -18,10 +18,15 @@
     <link href="https://fonts.googleapis.com/css?family=Lato:100,100i,300,300i,400,400i,700,700i,900,900i" rel="stylesheet">
 
     <!-- Custom styles for this template -->
-    <link href="css/one-page-wonder.min.css" rel="stylesheet">
+    <link href="98_css/one-page-wonder.min.css" rel="stylesheet">
 
-    <link href="CustomCss/myCss.css" rel="stylesheet">
+    <link href="00_resources/00_css/00_mainPage/mainCss.css" rel="stylesheet">
+    <link href="00_resources/00_css/00_mainPage/logInCss.css" rel="stylesheet">
 
+    <!-- Fonts Awesome -->
+    <script defer src="https://use.fontawesome.com/releases/v5.0.6/js/all.js"></script>
+
+    <?php session_start() ?>
   </head>
 
   <body>
@@ -30,14 +35,17 @@
     <nav class="navbar navbar-expand-lg navbar-dark navbar-custom fixed-top">
       <div class="container">
         <a class="navbar-brand" href="#">Crunchy</a>
+        <div class="searchBox">
+          <input class="searchTxt" type="text" name="search" value="search" placeholder="Search">
+          <a class="searchButton" href="#">
+            <i class="fas fa-search"></i>
+          </a>
+        </div>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarResponsive">
-          <ul class="navbar-nav ml-auto">
-            <li class="nav-item">
-              <a class="nav-link" href="index.php">Accueil</a>
-            </li>
+          <ul class="navbar-nav ml-auto"> 
             <li class="nav-item">
               <a class="nav-link" href="#">Ajouter une recette</a>
             </li>
@@ -45,7 +53,7 @@
               <a class="nav-link" href="#">Contact</a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="#">Inscription</a>
+              <a class="nav-link" href="01_src/signIn/signIn.php">Inscription</a>
             </li>
             <li class="nav-item">
               <a class="nav-link" href="#">Connexion</a>
@@ -60,7 +68,7 @@
         <div class="container">
           <h1 class="masthead-heading mb-0">Des délicieuses recettes</h1>
           <h2 class="masthead-subheading mb-0">Pour tous les goûts</h2>
-          <a href="Dishes/dishes.php" class="btn btn-primary btn-xl rounded-pill mt-5">Les recettes</a>
+          <a href="01_src/listDishes/dishes.php" class="btn btn-primary btn-xl rounded-pill mt-5">Les recettes</a>
         </div>
       </div>
       <div class="bg-circle-1 bg-circle"></div>
@@ -97,6 +105,26 @@
       }
     ?>
 
+
+    <!-- LogIn Fixed -->
+    <!-- php if SESSION["IsLoged"] == TRUE -->
+    <div class="display-logIn-container">
+        <i class="fas fa-angle-double-left rotate-left"></i>
+    </div>
+    <!--  -->
+    <div class="logIn-container-before">
+        <div class="text-LogIn">
+            <p>Log In</p>
+        </div>
+        <div class="form-LogIn">
+            <form action="index.php" method="post">
+                <input type="text" placeholder="Login"><br>
+                <input type="password" placeholder="password"><br>
+                <button type="submit">Log In</button>
+            </form>
+        </div>
+    </div>
+
     <!-- Footer -->
     <footer class="py-5 bg-red">
       <div class="container">
@@ -108,5 +136,15 @@
     <!-- Bootstrap core JavaScript -->
     <script src="vendor/jquery/jquery.min.js"></script>
     <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script type="text/javascript">
+        var toggleClassLogIn = function() {
+                $('.logIn-container-before').toggleClass('logIn-container-after');
+                $('.rotate-left').toggleClass('rotate-right');
+            };
+
+        $('.display-logIn-container').click(function() {
+            toggleClassLogIn();
+        });
+    </script>
   </body>
 </html>
