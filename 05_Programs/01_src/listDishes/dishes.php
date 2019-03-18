a8229294dae3b145db9a9ddc5223b424abd7d236
Levêque Loris <levequelo@etml.educanet2.ch>
2019-03-18 10:32:07 +0100

    Made the indent and the comment
    Miss one file (specialDish)

--- 05_Programs/01_src/listDishes/dishes.php
+++ 05_Programs/01_src/listDishes/dishes.php
@@ -1,162 +1,163 @@
 <!DOCTYPE html>
 <html lang="fr">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="">
-    <meta name="author" content="">
+        <head>
+        <!-- meta informations -->
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <meta name="description" content="page for the project 040 - ETML">
+        <meta name="author" content="dossantogo - levequelo">
 
-    <title>Crunchy</title>
+        <!-- Title -->
+        <title>Crunchy</title>
 
-    <!-- Bootstrap core CSS -->
-    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+        <!-- Bootstrap core CSS -->
+        <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
 
-    <!-- Custom styles for this template -->
-    <link href="css/1-col-portfolio.css" rel="stylesheet">
+        <!-- Custom styles for this template -->
+        <link href="css/1-col-portfolio.css" rel="stylesheet">
 
-    <!-- My customs styles -->
-    <link href="../../00_resources/00_css/01_dishes/dishesCss.css" rel="stylesheet">
-    <link href="../../00_resources/00_css/99_Universal/logIn.css" rel="stylesheet">
-    <link href="../../00_resources/00_css/99_Universal/searchBox.css" rel="stylesheet">
-    <link href="../../00_resources/00_css/01_dishes/searchBox.css" rel="stylesheet">
+        <!-- My customs styles -->
+        <link href="../../00_resources/00_css/01_dishes/dishesCss.css" rel="stylesheet">
+        <link href="../../00_resources/00_css/99_Universal/logIn.css" rel="stylesheet">
+        <link href="../../00_resources/00_css/99_Universal/searchBox.css" rel="stylesheet">
+        <link href="../../00_resources/00_css/01_dishes/searchBox.css" rel="stylesheet">
 
-    <!-- Fonts Awesome -->
-    <script defer src="https://use.fontawesome.com/releases/v5.0.6/js/all.js"></script>
+        <!-- Fonts Awesome -->
+        <script defer src="https://use.fontawesome.com/releases/v5.0.6/js/all.js"></script>
 
-  </head>
-
-  <body>
-
-    <!-- Navigation -->
-    <nav class="navbar navbar-expand-lg navbar-dark bg-red fixed-top">
-      <div class="container">
-        <a class="navbar-brand" href="../../index.php">Crunchy</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarResponsive">
-          <ul class="navbar-nav ml-auto">
-            <li class="nav-item">
-              <a class="nav-link" href="../../index.php">Accueil</a>
-            </li>
-            <!-- <balisephp>
-              if ($_SESSION["IsAdmin"] == true) {
+        <!-- php current session -->
+        <?php session_start() ?>
+    </head>
+    <body>
+        <!-- Navigation -->
+        <nav class="navbar navbar-expand-lg navbar-dark bg-red fixed-top">
+          <div class="container">
+            <a class="navbar-brand" href="../../index.php">Crunchy</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarResponsive">
+              <ul class="navbar-nav ml-auto">
                 <li class="nav-item">
-                <a class="nav-link" href="#">Ajouter une recette</a>
+                  <a class="nav-link" href="../../index.php">Accueil</a>
                 </li>
-              }
-            -->
-            <li class="nav-item">
-              <a class="nav-link" href="#">Contact</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="../signIn/signIn.php">Inscription</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Connexion</a>
-            </li>
-          </ul>
+                <!-- <balisephp>
+                  if ($_SESSION["IsAdmin"] == true) {
+                    <li class="nav-item">
+                    <a class="nav-link" href="#">Ajouter une recette</a>
+                    </li>
+                  }
+                -->
+                <li class="nav-item">
+                  <a class="nav-link" href="#">Contact</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="../signIn/signIn.php">Inscription</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#">Connexion</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </nav>
+        <!-- _===== SearchBox =====_ -->
+        <div class="searchBox">
+          <input class="searchTxt" type="text" name="earch" placeholder="Search">
+          <a class="searchButton" href="#">
+            <i class="fas fa-search"></i>
+          </a>
         </div>
-      </div>
-    </nav>
-    <!-- _===== SearchBox =====_ -->
-    <div class="searchBox">
-      <input class="searchTxt" type="text" name="earch" placeholder="Search">
-      <a class="searchButton" href="#">
-        <i class="fas fa-search"></i>
-      </a>
-    </div>
 
-    <!-- Page Content -->
-    <div class="container">
+        <!-- Page Content -->
+        <div class="container">
 
-      <!-- Page Heading -->
-      <h1 class="my-4">Recettes
-        <small>par ordre alphabétique</small>
-      </h1>
+            <!-- Page Heading -->
+            <h1 class="my-4">Recettes
+               <small>par ordre alphabétique</small>
+            </h1>
 
-      <?php
+            <?php
 
-       $arrayName = array('poulet','salades');
+                $arrayName = array('poulet','salades');
 
-      for ($i=0; $i < count($arrayName); $i++) {
-          echo '<div class="row">';
-          echo '<div class="col-md-7">';
-            echo '<a href="#">';
-              echo '<img class="img-fluid rounded mb-3 mb-md-0" src="http://placehold.it/700x300" alt="">';
-            echo '</a>';
-          echo '</div>';
-          echo '<div class="col-md-5">';
-            echo '<h3>Project</h3>';
-            echo '<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laudantium veniam exercitationem expedita laborum at voluptate. Labore, voluptates totam at aut nemo deserunt rem magni pariatur quos perspiciatis atque eveniet unde.</p>';
-            echo '<button class="btnPerso"><span>Voir recette</span></button>';
-          echo '</div>';
-        echo '</div>';
-        echo '<hr>';
-      }
-      ?>
+                for ($i=0; $i < count($arrayName); $i++) {
+                    echo '<div class="row">';
+                        echo '<div class="col-md-7">';
+                            echo '<a href="#">';
+                                echo '<img class="img-fluid rounded mb-3 mb-md-0" src="http://placehold.it/700x300" alt="">';
+                            echo '</a>';
+                        echo '</div>';
+                        echo '<div class="col-md-5">';
+                            echo '<h3>Project</h3>';
+                            echo '<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laudantium veniam exercitationem expedita laborum at voluptate. Labore, voluptates totam at aut nemo deserunt rem magni pariatur quos perspiciatis atque eveniet unde.</p>';
+                            echo '<button class="btnPerso"><span>Voir recette</span></button>';
+                        echo '</div>';
+                    echo '</div>';
+                    echo '<br>';
+                }
+            ?>
 
-      <!-- LogIn Fixed -->
-      <!-- php if SESSION["IsLoged"] == TRUE -->
-      <div class="display-logIn-container">
-          <i class="fas fa-angle-double-left rotate-left"></i>
-      </div>
-      <!--  -->
-      <div class="logIn-container-before">
-          <div class="text-logIn">
-              <p>Log In</p>
-          </div>
-          <div class="form-logIn">
-              <form action="index.php" method="post">
-                  <input type="text" placeholder="Login"><br>
-                  <input type="password" placeholder="password"><br>
-                  <button type="submit">Log In</button>
-              </form>
-          </div>
-      </div>
+            <!-- LogIn Fixed -->
+            <!-- php if SESSION["IsLoged"] == TRUE -->
+            <div class="display-logIn-container">
+                <i class="fas fa-angle-double-left rotate-left"></i>
+            </div>
+            <!-- end if -->
+            <!-- if the display button isn't here, we cant access into the form -->
+            <!-- but it's more securised the take this into the if -->
+            <div class="logIn-container-before">
+                <div class="text-logIn">
+                    <p>Log In</p>
+                </div>
+                <div class="form-logIn">
+                    <form action="index.php" method="post">
+                        <input type="text" placeholder="Login"><br>
+                        <input type="password" placeholder="password"><br>
+                        <button type="submit">Log In</button>
+                    </form>
+                </div>
+            </div>
 
-      <!-- Pagination -->
-      <ul class="pagination justify-content-center">
-        <li class="page-item">
-          <a class="page-link" href="#" aria-label="Previous">
-            <span aria-hidden="true">&laquo;</span>
-            <span class="sr-only">Previous</span>
-          </a>
-        </li>
-        <li class="page-item">
-          <a class="page-link" href="#">1</a>
-        </li>
-        <li class="page-item">
-          <a class="page-link" href="#">2</a>
-        </li>
-        <li class="page-item">
-          <a class="page-link" href="#">3</a>
-        </li>
-        <li class="page-item">
-          <a class="page-link" href="#" aria-label="Next">
-            <span aria-hidden="true">&raquo;</span>
-            <span class="sr-only">Next</span>
-          </a>
-        </li>
-      </ul>
+          <!-- Pagination -->
+          <ul class="pagination justify-content-center">
+                <li class="page-item">
+                    <a class="page-link" href="#" aria-label="Previous">
+                        <span aria-hidden="true">&laquo;</span>
+                        <span class="sr-only">Previous</span>
+                    </a>
+                </li>
+                <li class="page-item">
+                    <a class="page-link" href="#">1</a>
+                </li>
+                <li class="page-item">
+                    <a class="page-link" href="#">2</a>
+                </li>
+                <li class="page-item">
+                    <a class="page-link" href="#">3</a>
+                </li>
+                <li class="page-item">
+                    <a class="page-link" href="#" aria-label="Next">
+                        <span aria-hidden="true">&raquo;</span>
+                        <span class="sr-only">Next</span>
+                    </a>
+                </li>
+            </ul>
+        </div>
+        <!-- /.container -->
 
-    </div>
-    <!-- /.container -->
+        <!-- Footer -->
+        <footer class="py-5 bg-red">
+            <div class="container">
+                <p class="m-0 text-center text-white">Copyright &copy; Crunchy 2018</p>
+            </div>
+            <!-- /.container -->
+        </footer>
 
-    <!-- Footer -->
-    <footer class="py-5 bg-red">
-      <div class="container">
-        <p class="m-0 text-center text-white">Copyright &copy; Crunchy 2018</p>
-      </div>
-      <!-- /.container -->
-    </footer>
-
-    <!-- Bootstrap core JavaScript -->
-    <script src="vendor/jquery/jquery.min.js"></script>
-    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-    <script type="text/javascript" src="../../00_resources/03_js/99_Universal/logIn_Event.js"></script>
-    <script type="text/javascript" src="../../00_resources/03_js/99_Universal/searchBox_ColorChanger.js"> </script>
-
-  </body>
-
+        <!-- Bootstrap core JavaScript -->
+        <script src="vendor/jquery/jquery.min.js"></script>
+        <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+        <script type="text/javascript" src="../../00_resources/03_js/99_Universal/logIn_Event.js"></script>
+        <script type="text/javascript" src="../../00_resources/03_js/99_Universal/searchBox_ColorChanger.js"> </script>
+    </body>
 </html>
--- 05_Programs/index.php
+++ 05_Programs/index.php
@@ -1,110 +1,113 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
+    <head>
+        <!-- meta informations -->
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <meta name="description" content="page for the project 040 - ETML">
+        <meta name="author" content="dossantogo - levequelo">
 
-  <head>
+        <!-- title -->
+        <title>Crunchy</title>
 
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="">
-    <meta name="author" content="">
+        <!-- Bootstrap core CSS -->
+        <link href="99_vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
 
-    <title>Crunchy</title>
+        <!-- Custom fonts for this template -->
+        <link href="https://fonts.googleapis.com/css?family=Catamaran:100,200,300,400,500,600,700,800,900" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=Lato:100,100i,300,300i,400,400i,700,700i,900,900i" rel="stylesheet">
 
-    <!-- Bootstrap core CSS -->
-    <link href="99_vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+        <!-- Custom styles for this template -->
+        <link href="98_css/one-page-wonder.min.css" rel="stylesheet">
 
-    <!-- Custom fonts for this template -->
-    <link href="https://fonts.googleapis.com/css?family=Catamaran:100,200,300,400,500,600,700,800,900" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css?family=Lato:100,100i,300,300i,400,400i,700,700i,900,900i" rel="stylesheet">
+        <!-- customs styles -->
+        <link href="00_resources/00_css/99_Universal/searchBox.css" rel="stylesheet">
+        <link href="00_resources/00_css/99_Universal/logIn.css" rel="stylesheet">
+        <link href="00_resources/00_css/00_index/searchBox.css" rel="stylesheet">
 
-    <!-- Custom styles for this template -->
-    <link href="98_css/one-page-wonder.min.css" rel="stylesheet">
+        <!-- Fonts Awesome -->
+        <script defer src="https://use.fontawesome.com/releases/v5.0.6/js/all.js"></script>
 
-    <link href="00_resources/00_css/99_Universal/searchBox.css" rel="stylesheet">
-    <link href="00_resources/00_css/99_Universal/logIn.css" rel="stylesheet">
-    <link href="00_resources/00_css/00_index/searchBox.css" rel="stylesheet">
-
-    <!-- Fonts Awesome -->
-    <script defer src="https://use.fontawesome.com/releases/v5.0.6/js/all.js"></script>
-
-    <?php session_start() ?>
-  </head>
-
+        <!-- php current session -->
+        <?php session_start() ?>
+    </head>
   <body>
-
     <!-- Navigation -->
     <nav class="navbar navbar-expand-lg navbar-dark navbar-custom fixed-top">
-      <div class="container">
-        <a class="navbar-brand" href="#">Crunchy</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarResponsive">
-          <ul class="navbar-nav ml-auto">
-            <li class="nav-item">
-              <a class="nav-link" href="#">Ajouter une recette</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Contact</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="01_src/signIn/signIn.php">Inscription</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Connexion</a>
-            </li>
-          </ul>
+        <div class="container">
+            <a class="navbar-brand" href="#">Crunchy</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <!-- navbar link responsive -->
+            <div class="collapse navbar-collapse" id="navbarResponsive">
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Ajouter une recette</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Contact</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="01_src/signIn/signIn.php">Inscription</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Connexion</a>
+                    </li>
+                </ul>
+            </div>
         </div>
-      </div>
     </nav>
+
     <!-- _===== SearchBox =====_ -->
     <div class="searchBox">
-      <input class="searchTxt" type="text" name="earch" placeholder="Search">
-      <a class="searchButton" href="#">
-        <i class="fas fa-search"></i>
-      </a>
+        <input class="searchTxt" type="text" name="earch" placeholder="Search">
+        <a class="searchButton" href="#">
+            <i class="fas fa-search"></i>
+        </a>
     </div>
 
+    <!-- Page title / the header -->
     <header class="masthead text-center text-white">
-      <div class="masthead-content">
-        <div class="container">
-          <h1 class="masthead-heading mb-0">Des délicieuses recettes</h1>
-          <h2 class="masthead-subheading mb-0">Pour tous les goûts</h2>
-          <a href="01_src/listDishes/dishes.php" class="btn btn-primary btn-xl rounded-pill mt-5">Les recettes</a>
+        <div class="masthead-content">
+            <div class="container">
+                <h1 class="masthead-heading mb-0">Des délicieuses recettes</h1>
+                <h2 class="masthead-subheading mb-0">Pour tous les goûts</h2>
+                <a href="01_src/listDishes/dishes.php" class="btn btn-primary btn-xl rounded-pill mt-5">Les recettes</a>
+            </div>
         </div>
-      </div>
-      <div class="bg-circle-1 bg-circle"></div>
-      <div class="bg-circle-2 bg-circle"></div>
-      <div class="bg-circle-3 bg-circle"></div>
-      <div class="bg-circle-4 bg-circle"></div>
+        <div class="bg-circle-1 bg-circle"></div>
+        <div class="bg-circle-2 bg-circle"></div>
+        <div class="bg-circle-3 bg-circle"></div>
+        <div class="bg-circle-4 bg-circle"></div>
     </header>
 
     <?php
-      // Read Form db
-      $arrayType = array('poulet','salades');
-      $arrayImage = array('poulet','salades');
-      $arrayDesc = array('wallah1', 'wallah2');
+        // Read Form db
+        $arrayType = array('poulet','salades');
+        $arrayImage = array('poulet','salades');
+        $arrayDesc = array('wallah1', 'wallah2');
 
-      // show the value from arrays
-      for ($i=0; $i < count($arrayType); $i++) {
-         echo '<section>' ;
-            echo '<div class="container">';
-               echo ' <div class="row align-items-center">';
-                  echo '<div class="col-lg-6 order-lg-2">';
-                     echo '<div class="p-5">';
-                        echo '<img class="img-fluid rounded-circle" src="00_resources/01_images/00_mainPage/'.$arrayImage[$i].'.jpg" alt="">';
-                     echo '</div>';
-                  echo '</div>';
-                  echo '<div class="col-lg-6 order-lg-1">';
-                     echo ' <div class="p-5">';
-                        echo '<h2 class="display-4">'.$arrayType[$i].'</h2>';
-                        echo '<p>'.$arrayDesc[$i].'</p>';
-                     echo '</div>';
-                  echo '</div>';
-               echo '</div>';
-            echo '</div>';
-         echo '</section>';
-      }
+        // show the value from arrays
+        for ($i=0; $i < count($arrayType); $i++) {
+            echo '<section>' ;
+                echo '<div class="container">';
+                    echo ' <div class="row align-items-center">';
+                        echo '<div class="col-lg-6 order-lg-2">';
+                            echo '<div class="p-5">';
+                                echo '<img class="img-fluid rounded-circle" src="00_resources/01_images/00_mainPage/'.$arrayImage[$i].'.jpg" alt="">';
+                            echo '</div>';
+                        echo '</div>';
+                        echo '<div class="col-lg-6 order-lg-1">';
+                            echo ' <div class="p-5">';
+                                echo '<h2 class="display-4">'.$arrayType[$i].'</h2>';
+                                echo '<p>'.$arrayDesc[$i].'</p>';
+                            echo '</div>';
+                        echo '</div>';
+                    echo '</div>';
+                echo '</div>';
+            echo '</section>';
+        }
     ?>
 
 
@@ -113,7 +116,9 @@
     <div class="display-logIn-container">
         <i class="fas fa-angle-double-left rotate-left"></i>
     </div>
-    <!--  -->
+    <!-- end if -->
+    <!-- if the display button isn't here, we cant access into the form -->
+    <!-- but it's more securised the take this into the if -->
     <div class="logIn-container-before">
         <div class="text-logIn">
             <p>Log In</p>
@@ -138,6 +143,8 @@
     <!-- Bootstrap core JavaScript -->
     <script src="99_vendor/jquery/jquery.min.js"></script>
     <script src="99_vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+    <!-- customs javascripts -->
     <script type="text/javascript" src="00_resources/03_js/99_Universal/logIn_Event.js"></script>
     <script type="text/javascript" src="00_resources/03_js/99_Universal/searchBox_ColorChanger.js"> </script>
   </body>
