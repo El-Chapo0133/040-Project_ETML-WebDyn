5d6350dbc96f1824b1c6818816109dd37fa0a314
Levêque Loris <levequelo@etml.educanet2.ch>
2019-04-14 15:30:48 +0200

    Added index.html
    with a redirect index index.php
    index.php moved into a folder on 01_src

--- 05_Programs/01_src/specialDish/index.php
+++ 05_Programs/01_src/specialDish/index.php
@@ -40,14 +40,14 @@
     <!-- Navigation -->
     <nav class="navbar navbar-expand-lg navbar-dark bg-red fixed-top">
       <div class="container">
-        <a class="navbar-brand" href="#">Crunchy</a>
+        <a class="navbar-brand" href="..\index\index.php">Crunchy</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarResponsive">
           <ul class="navbar-nav ml-auto">
             <li class="nav-item active">
-              <a class="nav-link" href="..\..\index.php">Accueil</a>
+              <a class="nav-link" href="..\index\index.php">Accueil</a>
             </li>
             <li class="nav-item">
               <a class="nav-link" href="#">Ajouter une recette</a>
